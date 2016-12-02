abaa8561d284d9cd90c8735c4b24103b756869f4
eskca <eberedug@my.bridgeport.edu>
2016-12-02 01:15:29 -0500

    added Robo Test

---
+++ RoboTest/RoboTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RobotController;
+using Moq;
+using Ninject.MockingKernel.Moq;
+using System.ComponentModel.DataAnnotations;
+using Ninject;
+
+namespace RoboTest
+{
+    [TestClass]
+    public class RoboTest
+    {
+
+        private readonly MoqMockingKernel _kernel;
+
+        public RoboTest()
+        {
+            _kernel = new MoqMockingKernel();
+            _kernel.Bind<IRobotMove<Robot>>().To<Robot>();
+            _kernel.Bind<IRobotReact>().To<Robot>();
+        }
+
+        
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void gridCordinatesOutOfRange()
+        {
+           
+            var _con = _kernel.Get<Controller>();
+            _con.initializeGrid(7);
+            Robot bot = _con.startRobot(7, 8, "llrrll");
+            _con.moveRobot(bot);
+          
+            //auto assert handled by expectedException attribute
+        }
+
+       
+
+    }
+}
